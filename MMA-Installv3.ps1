b7b4360aeab6196e55fadbaa846f4ba3a845180a
Jack M. Lilley <37810857+JackLilley@users.noreply.github.com>
2021-09-04 13:19:18 -0500

    Update MMA-Installv3.ps1

--- MMA-Installv3.ps1
+++ MMA-Installv3.ps1
@@ -11,5 +11,5 @@ Invoke-WebRequest -Uri $source -OutFile $destination
 Set-Location "C:\windows\temp\"
 Start-Process -FilePath {C:\windows\Temp\MMASetup-AMD64.exe} -ArgumentList "/c /t:C:\windows\temp\setup"
 Start-Sleep -Seconds 10
-Start-Process -FilePath {C:\windows\Temp\setup\setup.exe} -ArgumentList "/qn NOAPM=1 ADD_OPINSIGHTS_WORKSPACE=1 OPINSIGHTS_WORKSPACE_AZURE_CLOUD_TYPE=1 OPINSIGHTS_WORKSPACE_ID='8cb82737-8851-4337-8cb8-7d6b0a93c180' OPINSIGHTS_WORKSPACE_KEY='uvBDm7TpQJCHQ/NprZSpVcUYFQm6aipTt4URBnhddBc37E4Nu5jmUAROalBIXOe97uqWRA1RMo9Cr5k+yFdskw==' AcceptEndUserLicenseAgreement=1"
+Start-Process -FilePath {C:\windows\Temp\setup\setup.exe} -ArgumentList "/qn NOAPM=1 ADD_OPINSIGHTS_WORKSPACE=1 OPINSIGHTS_WORKSPACE_AZURE_CLOUD_TYPE=1 OPINSIGHTS_WORKSPACE_ID='WORKSPACE-ID' OPINSIGHTS_WORKSPACE_KEY='WORKSPACE_KEY' AcceptEndUserLicenseAgreement=1"
 Write-Output "Install Complete"
